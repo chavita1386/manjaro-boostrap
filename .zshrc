40742d38d29e1c9ddd0a2d006a27ab3981cca2de
Chavita1386 <chavita1386@gmail.com>
2020-03-04 13:32:44 -0600

    Update files to new installation

--- .zshrc
+++ .zshrc
@@ -2,22 +2,32 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 export ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="wuffers"
-plugins=(git tig docker docker-compose golang pip)
-plugins+=( history history-substring-search httpie sudo vagrant postgres )
-plugins+=( osx lein node npm jump gulp mosh )
-plugins+=( k z alias-tips zsh-completions almostontop zsh-autosuggestions )
-plugins+=( zsh-syntax-highlighting )
+plugins=(
+    git
+    go
+    golang 
+    history
+    history-substring-search
+    sudo
+    osx
+    lein
+    node
+    npm
+    jump    
+    mosh
+    k
+    z    
+    zsh-completions
+    almostontop
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
 
 source $ZSH/oh-my-zsh.sh
 
 ## ALIAS
 alias c="clear"
 alias ez="vi $HOME/.zshrc"
-alias server="python -m SimpleHTTPServer"
-alias socks="ssh -vND 8888 kim"
-alias dpsa="docker ps -a"
-alias dcup="docker-compose up"
-alias dcupd="docker-compose up -d"
 
 ## GIT
 GIT_AUTHOR_NAME="Chavita1386"
@@ -26,8 +36,6 @@ git config --global user.name "$GIT_AUTHOR_NAME"
 GIT_AUTHOR_EMAIL="chavita1386@gmail.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
-# Set a default editor to avoid "Could not execute editor" error
-#git config --global core.editor "/usr/bin/vim"
 
 ## NPM
 npm set init.author.email "chavita1386@gmail.com"
@@ -47,10 +55,6 @@ export PATH=$PATH:$M2
 ## GO
 export GOPATH=$HOME/code/go
 export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
-
-# Make zsh know about hosts already accessed by SSH
-zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
-
 
 ## CUSTOM FUNCTIONS
 
--- boostrap.sh
+++ boostrap.sh
@@ -3,20 +3,15 @@
 # ask for password before all the scripts
 sudo echo "ask for password before all the scripts"
 
-## Install wget
-yes | sudo pacman -S wget git vim neovim chromium opera
-## Install JDK super Java de shit
-yes | pacman -S jdk11-openjdk
+## Install neccesary to clean installation
+yes | sudo pacman -S wget git vim neovim chromium opera zsh
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# install oh-my-zsh plugins
+git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips && git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/custom/plugins/k && git clone https://github.com/rupa/z $HOME/.oh-my-zsh/custom/plugins/z && git clone https://github.com/Valiev/almostontop.git $HOME/.oh-my-zsh/custom/plugins/almostontop && git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions && git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-## Install nodejs and npm
-yes | pacman -S nodejs
-yes | pacman -S npm
-
-## Install golang
-yes | pacman -S go
-
-## Install vim
-yes | pacman -S vim
+## Development languages
+yes | pacman -S jdk11-openjdk nodejs npm go 
 
 # FONTS powerline and fira code
 yes | pacman -S powerline-fonts
@@ -24,6 +19,9 @@ yes | pacman -S ttf-fira-code
 
 # setup dev env
 mkdir -p $HOME/code
+mkdir -p $HOME/code/java
+mkdir -p $HOME/code/go
+mkdir -p $HOME/code/js
 
 ## Config npm globals
 mkdir ~/.npm-global
@@ -32,19 +30,5 @@ touch ~/.profile
 echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
 source ~/.profile
 
-# install oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-# set .zshrc
+# set default github .zshrc file
 wget -O $HOME/.zshrc https://raw.githubusercontent.com/chavita1386/manjaro-boostrap/master/.zshrc
-
-# install oh-my-zsh plugins
-git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips && git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/custom/plugins/k && git clone https://github.com/rupa/z $HOME/.oh-my-zsh/custom/plugins/z && git clone https://github.com/Valiev/almostontop.git $HOME/.oh-my-zsh/custom/plugins/almostontop && git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions && git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-
-# pure prompt & other goodies
-#npm install -g pure-prompt browser-sync eslint diff-so-fancy ntl ghwd
-    
-## MAC CLI
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
-
