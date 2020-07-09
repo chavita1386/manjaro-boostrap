e9da20ee94e9dbaa43e7569006a0943d3baa0649
Chavita1386 <chavita1386@gmail.com>
2020-07-09 13:40:20 -0500

    Add yarn to boostrap file

--- boostrap.sh
+++ boostrap.sh
@@ -11,7 +11,7 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips && git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/custom/plugins/k && git clone https://github.com/rupa/z $HOME/.oh-my-zsh/custom/plugins/z && git clone https://github.com/Valiev/almostontop.git $HOME/.oh-my-zsh/custom/plugins/almostontop && git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions && git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ## Development languages
-yes | pacman -S jdk11-openjdk nodejs npm go 
+yes | pacman -S jdk11-openjdk nodejs npm go yarn
 
 # FONTS powerline and fira code
 yes | pacman -S powerline-fonts ttf-fira-code
